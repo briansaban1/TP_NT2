7f19780f234b0ca3de653b329cb3c6ac3d3e2836
Brian <info.tododeusa@gmail.com>
2021-11-10 01:20:03 -0300

    se listan los servicios destacados en el Home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,19 +1,66 @@
 <template>
   <div>
     <Slider />
-    <Cuadros />
+
+    <div style="margin-top:25px">
+      <h2>Servicios destacados</h2>
+    </div>
+    <b-container>
+      <b-row>
+        <b-col
+          v-for="servicio in servicios.slice(0,8)"
+          :key="servicio.id"
+          cols="4"
+          sm="5"
+          md="5"
+          lg="3"
+          class="align-center"
+          
+        >
+          <CuadrosServicio :servicio="servicio" />
+        </b-col>
+      </b-row>
+      </b-container>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import Slider from '../components/Slider.vue'
-import Cuadros from '../components/Cuadros.vue'
+import CuadrosServicio from "../components/CuadrosServicio.vue";
+import axios from "axios";
+
 export default {
   name: 'Home',
   components: {
     Slider,
-    Cuadros
-  }
-}
+    CuadrosServicio
+  },
+  data() {
+    return {
+      barrio: "",
+      rubro: "",
+      servicios: [],
+      servicioSeleccionado: null,
+    };
+  },
+  methods: {
+    async getServicios() {
+      try {
+        const url = "https://618072ba8bfae60017adfaec.mockapi.io/servicio";
+        const response = await axios.get(url);
+        this.servicios = response.data;
+        console.log(this.servicios);
+      } catch (error) {
+        console.log(error);
+      }
+    },
+  },
+  computed: {
+
+},
+  created() {
+    this.getServicios();
+  },
+};
 </script>
