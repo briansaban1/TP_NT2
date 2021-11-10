e2711a88f9c5b4e644259ffc065420d24a8abad4
Brian <info.tododeusa@gmail.com>
2021-11-10 01:20:57 -0300

    se muestran 4 servicios por columna

--- src/views/Servicios.vue
+++ src/views/Servicios.vue
@@ -12,7 +12,7 @@
           cols="12"
           sm="6"
           md="4"
-          lg="4"
+          lg="3"
           class="align-center"
         >
           <CuadrosServicio :servicio="servicio" />
