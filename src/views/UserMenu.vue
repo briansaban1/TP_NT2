bf93a96f4c3d5ebe1aa79d7dbc00ec827f0a2c42
Brian <info.tododeusa@gmail.com>
2021-11-14 14:08:43 -0300

    se corrige error en validación de tipo de usuario

--- src/views/UserMenu.vue
+++ src/views/UserMenu.vue
@@ -1,19 +1,20 @@
 <template>
   <div id="userMenu">
     <div class="row" style="width: 100%">
-   <b-container style="width: 500px; margin-top:25px;">
+   <b-container style="width: 500px; margin-top:25px; ">
     <h1>Hola, {{ usuario.user }}!</h1>
     <p>Bienvenido a tu cuenta de ORTYFY-Service</p>
 
-    <div style="">
-      <p>Nombre: {{ usuario.nombre }}</p>
-      <p>Apellido: {{ usuario.apellido }}</p>
-      <p>Email: {{ usuario.email }}</p>
-      <p>Usuario: {{ usuario.user }}</p>
+    <div style="justify-content: left;">
+      <p style="display: flex; justify-content: left; margin-left: 20px">Nombre: {{ usuario.nombre }}</p>
+      <p style="display: flex; justify-content: left;margin-left: 20px">Apellido: {{ usuario.apellido }}</p>
+      <p style="display: flex; justify-content: left;margin-left: 20px">Email: {{ usuario.email }}</p>
+      <p style="display: flex; justify-content: left;margin-left: 20px">Usuario: {{ usuario.user }}</p>
+      <p style="display: flex; justify-content: left;margin-left: 20px">Tipo: {{ usuario.tipo }}</p>
       </div>
 </b-container>
 <b-container style="width: 300px; margin-top:15px; margin-left: 20px">
-    <div v-if="usuario.tipo = 'vendedor'">
+    <div v-if="usuario.tipo == 'vendedor'">
       <div style="margin: 20px">
         <router-link to="/CrearServicio" class="btn btn-primary btn-block"
           >Crear Servicio</router-link>
@@ -27,9 +28,20 @@
           >Eliminar Servicio</router-link>
       </div>
     </div>
-    <div v-else></div>
+    <div v-else>
+   <div style="margin: 20px">
+      <router-link to="/ServiciosFavoritos" class="btn btn-primary btn-block"
+          >Servicios Favoritos</router-link>
+    </div>
+
+    </div>
 
     <div style="margin: 20px">
+      <router-link to="/ModificarDatos" class="btn btn-primary btn-block"
+          >Modificar Datos</router-link>
+          </div>
+    <div style="margin: 20px">
+
       <b-button @click="logout" class="btn btn-primary btn-block"
         >Cerrar Sesión</b-button
       >
